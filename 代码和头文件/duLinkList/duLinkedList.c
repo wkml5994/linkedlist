6a661b756522993ae821e8257d1337df66087b9d
wk ml <email@example.com>
2021-03-27 22:18:17 +0800

    v1.0

---
+++ 代码和头文件/duLinkList/duLinkedList.c
@@ -0,0 +1,67 @@
+#include "../head/duLinkedList.h"
+
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status InitList_DuL(DuLinkedList *L) {
+
+}
+
+/**
+ *  @name        : void DestroyList_DuL(DuLinkedList *L)
+ *	@description : destroy a linked list
+ *	@param		 : L(the head node)
+ *	@return		 : status
+ *  @notice      : None
+ */
+void DestroyList_DuL(DuLinkedList *L) {
+
+}
+
+/**
+ *  @name        : Status InsertBeforeList_DuL(DuLNode *p, LNode *q)
+ *	@description : insert node q before node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status InsertBeforeList_DuL(DuLNode *p, DuLNode *q) {
+
+}
+
+/**
+ *  @name        : Status InsertAfterList_DuL(DuLNode *p, DuLNode *q)
+ *	@description : insert node q after node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status InsertAfterList_DuL(DuLNode *p, DuLNode *q) {
+
+}
+
+/**
+ *  @name        : Status DeleteList_DuL(DuLNode *p, ElemType *e)
+ *	@description : delete the first node after the node p and assign its value to e
+ *	@param		 : p, e
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status DeleteList_DuL(DuLNode *p, ElemType *e) {
+
+}
+
+/**
+ *  @name        : void TraverseList_DuL(DuLinkedList L, void (*visit)(ElemType e))
+ *	@description : traverse the linked list and call the funtion visit
+ *	@param		 : L(the head node), visit
+ *	@return		 : Status
+ *  @notice      : None
+ */
+void TraverseList_DuL(DuLinkedList L, void (*visit)(ElemType e)) {
+
+}
---
+++ 代码和头文件/duLinkList/main.c
@@ -0,0 +1,129 @@
+#include "duLinkedListcopy.h"
+int main()
+{
+	DuLinkedList head=NULL;
+	int input;
+	int sign=1;
+	while(sign) {
+		menu();
+		scanf("%d",&input);
+		switch(input) {
+				// init
+			case 1: {
+				system("cls");
+				if(InitList_DuL(&head)) {
+					printf("初始化链表成功！\n");
+				}
+				CreatList_DuL(&head);
+				fflush(stdin);
+				system("pause");
+				system("cls");
+				break;
+			}
+			// insertafter
+			case 2: {
+				system("cls");
+				DuLNode *p,*q;
+				int pre_data;
+				printf("请输入你要插入到的位置的前一个结点的数据：");
+				scanf("%d",&pre_data);
+				p = FindNode_DuL(pre_data,head);
+				if(!p) {	/* p is not found */
+					printf("没有找到你要插入的前节点！请检查输入！\n");
+				} else {
+					q = (DuLNode *)malloc(sizeof(DuLNode));
+					printf("请输入你要插入节点的数据：\n");
+					scanf("%d",&q->data);
+					q->prior = NULL;
+					q->next = NULL;
+					if(InsertAfterList_DuL(p, q)) {
+						printf("插入成功！\n");
+					} else {
+						printf("插入失败！\n");
+					}
+				}
+				fflush(stdin);
+				system("pause");
+				system("cls");
+				break;
+			}
+			// output
+			case 3: {
+				system("cls");
+				TraverseList_DuL(head,visit);
+				fflush(stdin);
+				system("pause");
+				system("cls");
+				break;
+			}
+			//insertbefore
+			case 4: {
+				system("cls");
+				DuLNode *p,*q;
+				int aft_data;
+				printf("请输入你要插入到的位置的后一个结点的数据：");
+				scanf("%d",&aft_data);
+				p = FindNode_DuL(aft_data,head);
+				if(!p) {	/* p is not found */
+					printf("没有找到你要插入的后节点！请检查输入！\n");
+				} else {
+					q = (DuLNode *)malloc(sizeof(DuLNode));
+					printf("请输入你要插入节点的数据：\n");
+					scanf("%d",&q->data);
+					q->prior =NULL;
+					q->next = NULL;
+					if(InsertBeforeList_DuL(p, q)) {
+						printf("插入成功！\n");
+					} else {
+						printf("插入失败！\n");
+					}
+				}
+				fflush(stdin);
+				system("pause");
+				system("cls");
+				break;
+			}
+			//delete
+			case 5: {
+				system("cls");
+				int pre_data;
+				DuLNode *p;
+				ElemType record,*recorded=&record;
+				printf("请输入你要删除后节点的前节点数据：");
+				scanf("%d",&pre_data);
+				p = FindNode_DuL(pre_data,head);
+				if(!p) { /* p is no found */
+					printf("没有找到你所输入的节点！\n");
+				} else {
+					if(DeleteList_DuL(p,recorded)) {
+						printf("已经成功删除数据：%d\n",*recorded);
+					} else {
+						printf("删除失败！\n");
+					}
+				}
+				fflush(stdin);
+				system("pause");
+				system("cls");
+				break;
+			}
+			// exit
+			case 0: {
+				system("cls");
+				DestroyList_DuL(&head);
+				fflush(stdin);
+				system("cls");
+				sign=0;
+				break;
+			}
+			default: {
+				printf("请输入1-9！\n");
+				fflush(stdin);
+				system("pause");
+				system("cls");
+			}
+		}
+	}
+	printf("感谢使用本系统！");
+	return 0;
+
+}
